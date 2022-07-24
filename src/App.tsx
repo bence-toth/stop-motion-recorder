b2048d987810c59ef68f263975a36e5c1ec5490b
Bence A. Tóth <tothab@gmail.com>
2022-07-24 21:36:12 +0200

    Implemented basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
-const App = () => <div></div>;
+import "./App.css";
+
+const App = () => (
+  <div className="app">
+    <header className="header">
+      <h1>Stop Motion Recorder</h1>
+      <span className="by-line">
+        Made with ❤️ by{" "}
+        <a
+          href="https://github.com/bence-toth/stop-motion-recorder"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bence A. Tóth
+        </a>
+      </span>
+    </header>
+    <main className="main"></main>
+    <aside className="sidebar"></aside>
+    <footer className="toolbar">
+      <div>
+        <button>Capture</button>
+      </div>
+      <div>
+        <button>Preview</button>
+      </div>
+    </footer>
+  </div>
+);
 
 export default App;
